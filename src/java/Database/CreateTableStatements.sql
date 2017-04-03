903829d3752f492eee2b215f9b55b3ba640a62be
Yomna <yomna.hassan@hotmail.com>
2017-04-03 11:08:04 +0400

    Database migrated back to localhost Apache Derby due to errors and slowness of Microsoft Azure SQL Server & DB

--- src/java/Database/CreateTableStatements.sql
+++ src/java/Database/CreateTableStatements.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ELCDuser
 (
-userID INT PRIMARY KEY IDENTITY (1,1),
+userID INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
 firstname VARCHAR(140),
 lastname VARCHAR(140),
 sex VARCHAR(1),
@@ -38,32 +38,32 @@ INSERT INTO Patient(patientID, doctorID) VALUES(3, 1);
 
 CREATE TABLE Comment
 (
-commentID INT PRIMARY KEY IDENTITY (1,1),
+commentID INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
 patientID INT,
 doctorID INT,
 comment VARCHAR(1000),
 dateAdded DATE,
-isVisible BIT,
+isVisible BOOLEAN,
 CONSTRAINT comment_patientID_fk FOREIGN KEY(patientID) REFERENCES Patient(patientID),
 CONSTRAINT comment_doctorID_fk FOREIGN KEY(doctorID) REFERENCES Doctor(doctorID)
 );
 
-INSERT INTO Comment(patientID, doctorID, comment, dateAdded, isVisible) VALUES(2, 1, 'Low-risk, but still needs to stop smoking', '2017-03-28', 1);
-INSERT INTO Comment(patientID, doctorID, comment, dateAdded, isVisible) VALUES(2, 1, 'If smoking does not stop, he may develop several types of cancers', '2017-03-28', 0);
-INSERT INTO Comment(patientID, doctorID, comment, dateAdded, isVisible) VALUES(3, 1, 'No worry at all! Good health :)', '2017-03-25', 1);
+INSERT INTO Comment(patientID, doctorID, comment, dateAdded, isVisible) VALUES(2, 1, 'Low-risk, but still needs to stop smoking', '2017-03-28', TRUE);
+INSERT INTO Comment(patientID, doctorID, comment, dateAdded, isVisible) VALUES(2, 1, 'If smoking does not stop, he may develop several types of cancers', '2017-03-28', FALSE);
+INSERT INTO Comment(patientID, doctorID, comment, dateAdded, isVisible) VALUES(3, 1, 'No worry at all! Good health :)', '2017-03-25', TRUE);
 
 CREATE TABLE Gene
 (
-geneID INT PRIMARY KEY IDENTITY (1,1),
+geneID INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
 patientID INT,
 doctorID INT,
 name VARCHAR(140),
 result VARCHAR(1000),
-isFatal BIT,
+isFatal BOOLEAN,
 CONSTRAINT gene_patientID_fk FOREIGN KEY(patientID) REFERENCES Patient(patientID),
 CONSTRAINT gene_doctorID_fk FOREIGN KEY(doctorID) REFERENCES Doctor(doctorID)
 );
 
-INSERT INTO Gene(patientID, doctorID, name, result, isFatal) VALUES(2, 1, 'Gene 1', 'This gene is a direct correspondent to lung cancer, better watch out', 1);
-INSERT INTO Gene(patientID, doctorID, name, result, isFatal) VALUES(3, 1, 'Gene 1', 'Not present', 0);
-INSERT INTO Gene(patientID, doctorID, name, result, isFatal) VALUES(3, 1, 'Gene 2', 'Must take care a little', 1);
+INSERT INTO Gene(patientID, doctorID, name, result, isFatal) VALUES(2, 1, 'Gene 1', 'This gene is a direct correspondent to lung cancer, better watch out', TRUE);
+INSERT INTO Gene(patientID, doctorID, name, result, isFatal) VALUES(3, 1, 'Gene 1', 'Not present', FALSE);
+INSERT INTO Gene(patientID, doctorID, name, result, isFatal) VALUES(3, 1, 'Gene 2', 'Must take care a little', TRUE);
